dbfbb2470aeef49e11d8d635d12503bf34fc1a75
Юрий Шаповалов <mr.Shapovaloff@gmail.com>
2020-08-03 05:23:54 +0300

    Добавляет валидацию формы

--- build/js/main.js
+++ build/js/main.js
@@ -11,6 +11,7 @@
   var questionPopup = document.querySelector('.popup--question');
   var questionClose = questionPopup.querySelector('.popup__close--question');
   var questionOverlay = questionPopup.querySelector('.popup__overlay');
+  var nameField = document.querySelector('input[type="text"]');
 
   var openTownPopup = function (popup, evt) {
     evt.preventDefault();
@@ -18,7 +19,7 @@
     popup.classList.add('popup--active');
   };
 
-  var closeTownPopup = function (popup, evt) {
+  window.closeTownPopup = function (popup, evt) {
     evt.preventDefault();
     bodyElement.classList.remove('no-scroll');
     popup.classList.remove('popup--active');
@@ -27,7 +28,7 @@
   window.addEventListener('keydown', function (evt) {
     if (evt.keyCode === ESCAPE) {
       if (townPopup.classList.contains('popup--active')) {
-        closeTownPopup(townPopup, evt);
+        window.closeTownPopup(townPopup, evt);
       }
     }
   });
@@ -35,7 +36,7 @@
   window.addEventListener('keydown', function (evt) {
     if (evt.keyCode === ESCAPE) {
       if (questionPopup.classList.contains('popup--active')) {
-        closeTownPopup(questionPopup, evt);
+        window.closeTownPopup(questionPopup, evt);
       }
     }
   });
@@ -43,12 +44,13 @@
   var listenerPopupOpen = function (popupButton, popup) {
     popupButton.addEventListener('click', function (evt) {
       openTownPopup(popup, evt);
+      nameField.focus();
     });
   };
 
   var listenerPopupClose = function (popupButton, popup) {
     popupButton.addEventListener('click', function (evt) {
-      closeTownPopup(popup, evt);
+      window.closeTownPopup(popup, evt);
     });
   };
 
@@ -60,11 +62,11 @@
 
 
   townOverlay.addEventListener('click', function (evt) {
-    closeTownPopup(townPopup, evt);
+    window.closeTownPopup(townPopup, evt);
   });
 
   questionOverlay.addEventListener('click', function (evt) {
-    closeTownPopup(questionPopup, evt);
+    window.closeTownPopup(questionPopup, evt);
   });
 
 })();
@@ -74,8 +76,8 @@
 (function () {
   var menuButton = document.querySelector('.page-header__toggle');
   var menuHeader = document.querySelector('.page-header');
-  var menuMain = document.querySelector('.page-main');
   var menuFooter = document.querySelector('.page-footer');
+  var bodyElement = document.querySelector('body');
 
   menuHeader.classList.remove('page-header--nojs');
   menuFooter.classList.remove('page-footer--nojs');
@@ -86,13 +88,13 @@
       menuFooter.classList.remove('page-footer--closed');
       menuHeader.classList.add('page-header--opened');
       menuFooter.classList.add('page-footer--opened');
-      menuMain.style.display = 'none';
+      bodyElement.classList.add('no-scroll');
     } else {
       menuHeader.classList.remove('page-header--opened');
       menuFooter.classList.remove('page-footer--opened');
       menuHeader.classList.add('page-header--closed');
       menuFooter.classList.add('page-footer--closed');
-      menuMain.style.display = 'block';
+      bodyElement.classList.remove('no-scroll');
     }
   });
 })();
@@ -133,4 +135,113 @@
 
   breakpoint.addListener(breakpointChecker);
   breakpointChecker();
+})();
+
+// Валидация формы
+
+(function () {
+  var form = document.querySelector('.popup__form');
+  var formButton = document.querySelector('.form__btn');
+  var inputs = form.querySelectorAll('input');
+  var textarea = form.querySelector('textarea');
+
+  var checkFormState = function () {
+    var isReady = true;
+
+    for (var i = 0; i < inputs.length; i++) {
+      var input = inputs[i];
+
+      if (input.type === 'checkbox' && !input.checked) {
+        isReady = false;
+      }
+
+      if (input.validity.tooShort) {
+        isReady = false;
+      }
+
+      if (textarea.validity.tooShort) {
+        isReady = false;
+      }
+
+      if (input.id === 'form__email' && input.validity.typeMismatch) {
+        isReady = false;
+      }
+    }
+    return isReady;
+  };
+
+  form.addEventListener('input', function (evt) {
+    var isReady = checkFormState();
+
+    if (isReady) {
+      formButton.disabled = false;
+    } else {
+      formButton.disabled = true;
+    }
+
+    var input = evt.target.closest('.form__input');
+    var inputBlock = evt.target.closest('.form__input-block');
+
+    if (input) {
+      var formInvalid = inputBlock.querySelector('.form__invalid');
+      var formPlaceholder = inputBlock.querySelector('.form__placeholder');
+
+      if (input.value) {
+        formPlaceholder.classList.add('form__placeholder--value');
+      } else {
+        formPlaceholder.classList.remove('form__placeholder--value');
+      }
+
+      if (input.id === 'form__name') {
+        if (input.validity.tooShort) {
+          formInvalid.style.display = 'block';
+          input.classList.add('form__input--invalid');
+        } else {
+          formInvalid.style.display = 'none';
+          input.classList.remove('form__input--invalid');
+        }
+      }
+
+      if (input.id === 'form__email') {
+        if (input.validity.typeMismatch) {
+          formInvalid.style.display = 'block';
+          input.classList.add('form__input--invalid');
+        } else {
+          formInvalid.style.display = 'none';
+          input.classList.remove('form__input--invalid');
+        }
+      }
+    }
+  });
+})();
+
+// Отправка формы и сохранения в localStoredg
+
+(function () {
+  var form = document.querySelector('.popup__form');
+  var questionPopup = document.querySelector('.popup--question');
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    var name = form.querySelector('input[type="text"]');
+    var email = form.querySelector('input[type="email"]');
+    var question = form.querySelector('textarea[name="question"]');
+    var checkbox = form.querySelector('input[type="checkbox"]');
+
+    var userQuestion = {
+      name: name.value,
+      email: email.value,
+      question: question.value,
+      checkbox: checkbox.checked
+    };
+
+    localStorage.setItem('userQuestion', JSON.stringify(userQuestion));
+
+    name.value = '';
+    email.value = '';
+    question.value = '';
+    checkbox.checked = false;
+
+    window.closeTownPopup(questionPopup, evt);
+  });
 })();
